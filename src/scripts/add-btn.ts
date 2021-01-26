6fbf57b7273d00e242e09582cd70734cb582d093
MHammadYou <MHammadY2.0@gmail.com>
2021-01-26 23:21:57 +0500

    added block styles

--- src/scripts/add-btn.ts
+++ src/scripts/add-btn.ts
@@ -2,6 +2,7 @@ const addBtn = document.querySelector<HTMLElement>("#add");
 const blocks = document.querySelector<HTMLElement>("#blocks");
 
 if (addBtn && blocks) {
+  blocks.classList.toggle("blocks-toggle");
   addBtn.addEventListener("click", () => {
     blocks.classList.toggle("blocks-toggle");
   });
